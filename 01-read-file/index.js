f1cee39ab1f669917605108817efc145ef10b7c1
Roman Gromov <gromov.r.v.92@gmail.com>
2024-01-20 00:06:25 +0500

    feat: 02-write-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,8 +1,6 @@
-const fileSystem = require('fs');
+const { createReadStream } = require('fs');
 const path = require('path');
 
 const filePath = path.join(__dirname, 'text.txt');
 
-fileSystem
-  .createReadStream(filePath, 'utf8')
-  .on('data', (chunk) => console.log(chunk));
+createReadStream(filePath, 'utf8').on('data', (chunk) => console.log(chunk));
---
+++ 02-write-file/index.js
@@ -0,0 +1,46 @@
+const fs = require('fs');
+const path = require('path');
+
+const { stdin, stdout } = process;
+
+const colors = {
+  default: '\x1b[0m',
+  green: '\x1b[32m',
+  yellow: '\x1b[33m',
+  blue: '\x1b[94m',
+  white: '\x1b[97m',
+};
+const fileName = 'text.txt';
+const intro = `${colors.green}The entered text will be added to text.txt file.
+${colors.yellow}To exit press CTRL+C, or type .exit <${colors.default}\n`;
+const goodbye = `\n${colors.blue}Have a good day!${colors.default}`;
+const continueStart = `\n${colors.green}The text:${colors.white}`;
+const continueEnd = `${colors.green}have written to a file, you can continue typing:${colors.default}\n`;
+
+fs.writeFile(path.join(__dirname, fileName), '', (error) => {
+  if (error) {
+    throw error;
+  }
+  stdout.write(intro);
+});
+
+process.on('SIGINT', () => {
+  stdout.write(goodbye);
+  process.exit();
+});
+
+stdin.on('data', (data) => {
+  if (data.toString().trim() === 'exit') {
+    stdout.write(goodbye);
+    process.exit();
+    return;
+  }
+
+  fs.appendFile(path.join(__dirname, fileName), data, (error) => {
+    if (error) {
+      throw error;
+    }
+  });
+
+  stdout.write(`${continueStart} ${data.toString().trim()} ${continueEnd}`);
+});
